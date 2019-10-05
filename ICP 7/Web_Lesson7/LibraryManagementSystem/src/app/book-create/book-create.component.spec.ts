77f56b886f4bba58b00b6a5d0b2bf0c7ecb1d4af
ThoshitaMovva <46982930+ThoshitaMovva@users.noreply.github.com>
2019-10-05 16:38:50 -0500

    ICP 7

---
+++ ICP 7/Web_Lesson7/LibraryManagementSystem/src/app/book-create/book-create.component.spec.ts
@@ -0,0 +1,26 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import 'jasmine'
+
+import {BookCreateComponent} from './book-create.component';
+
+describe('BookCreateComponent', () => {
+  let component: BookCreateComponent;
+  let fixture: ComponentFixture<BookCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BookCreateComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ICP 7/Web_Lesson7/LibraryManagementSystem/src/app/book-edit/book-edit.component.spec.ts
@@ -0,0 +1,26 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import 'jasmine'
+
+import {BookEditComponent} from './book-edit.component';
+
+describe('BookEditComponent', () => {
+  let component: BookEditComponent;
+  let fixture: ComponentFixture<BookEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BookEditComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
